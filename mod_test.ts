6808fe27f279091d061b38d16895929373e939ed
Shahid N. Shah <shahid@shah.org>
2021-08-04 13:00:12 -0400

    feat: add new utility types

--- mod_test.ts
+++ mod_test.ts
@@ -37,3 +37,99 @@ Deno.test(`type array guard`, () => {
     ),
   );
 });
+
+interface StronglyTyped {
+  text: string;
+  flag?: boolean;
+  numeric?: number;
+  required: string;
+}
+
+type FlagOrNumeric = mod.RequireAtLeastOne<
+  StronglyTyped,
+  "flag" | "numeric"
+>;
+
+Deno.test(`RequireAtLeastOne<StronglyTyped>`, () => {
+  const withFlag: FlagOrNumeric = {
+    text: "test1",
+    flag: true,
+    required: "test2",
+  };
+
+  const withNumeric: FlagOrNumeric = {
+    text: "test1",
+    numeric: 45,
+    required: "test2",
+  };
+
+  // to test: uncomment this to see an error
+  // const errorWithNeither: FlagOrNumeric = {
+  //   text: "test",
+  //   required: "icon",
+  // };
+
+  const noErrorWithBoth: FlagOrNumeric = {
+    text: "test1",
+    flag: false,
+    numeric: 37,
+    required: "test2",
+  };
+
+  // to test: uncomment this to see an error
+  // const errorWithBothWhenOneHasWrongType: FlagOrNumeric = {
+  //   text: "test1",
+  //   flag: false,
+  //   numeric: "should be a number here",
+  //   required: "test2",
+  // };
+
+  ta.assert(withFlag);
+  ta.assert(withNumeric);
+  ta.assert(noErrorWithBoth);
+});
+
+type OnlyOneFlagOrNumeric = mod.RequireOnlyOne<
+  StronglyTyped,
+  "flag" | "numeric"
+>;
+
+Deno.test(`RequireOnlyOne<StronglyTyped>`, () => {
+  const noErrorWithOnlyOne: OnlyOneFlagOrNumeric = {
+    text: "test1",
+    flag: true,
+    required: "test2",
+  };
+
+  // to test: uncomment this to see an error
+  // const errorWithBoth: OnlyOneFlagOrNumeric = {
+  //   text: "test1",
+  //   flag: false,
+  //   numeric: 53,
+  //   required: "test2",
+  // };
+
+  // This interface will be used to trick OnlyOneFlagOrNumeric into allowing an object with both
+  interface TrickStronglyTyped {
+    text: string;
+    numeric: number;
+    required: string;
+  }
+
+  const hasBoth = {
+    text: "test",
+    flag: true,
+    numeric: 24,
+    required: "test2",
+  };
+
+  // No error because excess properties are only disallowed when directly assigning an object literal
+  const temp: TrickStronglyTyped = hasBoth;
+
+  // No error despite temp actually having both because TS only knows that temp is a TrickStronglyTyped
+  const trickIntoAllowingBoth: OnlyOneFlagOrNumeric = temp;
+
+  ta.assert(noErrorWithOnlyOne);
+  ta.assert(temp);
+  ta.assert(trickIntoAllowingBoth);
+});
